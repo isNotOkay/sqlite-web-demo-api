f10c22193ddef494dac521bd416a2e7b22b57931
kayod <kay.odenthal@gmx.de>
2025-09-02 11:26:49 +0200

    add comment

--- SqliteWebDemoApi/Models/SqliteRelationInfo.cs
+++ SqliteWebDemoApi/Models/SqliteRelationInfo.cs
@@ -1,5 +1,9 @@
 ﻿namespace SqliteWebDemoApi.Models;
 
+/// <summary>
+/// Represents a relational object in SQLite.
+/// This can be either a <b>table</b> or a <b>view</b>.
+/// </summary>
 public sealed class SqliteRelationInfo
 {
     public required string Name { get; init; }
